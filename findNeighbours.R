199dccc62265b5a638341ac641993ad20403356d
James Boot <44063127+jamesboot@users.noreply.github.com>
2023-08-31 18:18:52 +0100

    Add files via upload

---
+++ findNeighbours.R
@@ -0,0 +1,34 @@
+# Identify barcodes of spots adjacent to each spot from tissue_positions file output from spaceranger
+
+# Create function
+findNeighbours <- function(file) {
+  
+  # Read in CSV
+  tissue_pos <- read.csv(file)
+  
+  # Make function to find neighbours apply
+  f <- function(x, list) {
+    bcode_lkup <- x[1]
+    row <- as.numeric(x[3])
+    col <- as.numeric(x[4])
+    
+    adjacent_bcodes <-
+      tissue_pos[abs(row - tissue_pos$array_row) + abs(col - tissue_pos$array_col) <= 2 &
+                   abs(row - tissue_pos$array_row) + abs(col - tissue_pos$array_col) > 0,]
+    
+    list[[bcode_lkup]] <- list(primaryBcode = bcode_lkup,
+                               neighbours = adjacent_bcodes$barcode)
+    
+  }
+  
+  # Apply function over rows (1) using apply
+  output_list <- list()
+  neighbours <- apply(tissue_pos, 1, FUN = f, list = output_list)
+  
+  # Name each element in the list by the barcode
+  names(neighbours) <- tissue_pos$barcode
+  
+  # Return the neighbours list
+  return(neighbours)
+  
+}
---
+++ loadDB.R
@@ -0,0 +1,72 @@
+# Function to read in the database from cellphonedb ready for analysis of adjacent spots
+loadDB <- function(gene.input,
+                   complex.input,
+                   interaction.input) {
+  
+  # Create blank list
+  cellphonedb <- list()
+  
+  # We need to load the gene input all file
+  # This allows us to look up gene names and find their uniprot ID
+  gene_lookup <-
+    read.csv(
+      '/Users/jamesboot/Documents/9.Genome Centre Files/interactVis/cellphonedb-data-4.0.0/data/gene_input_all.csv'
+    )
+  
+  # We need to load the complex input file
+  # This contains all info of the complexes - what proteins are involved
+  complex_lookup <-
+    read.csv(
+      '/Users/jamesboot/Documents/9.Genome Centre Files/interactVis/cellphonedb-data-4.0.0/data/complex_input.csv'
+    )
+  # Make the complex name the row names
+  row.names(complex_lookup) <- complex_lookup$complex_name
+  
+  # We need to load the interaction input file
+  # This contains the complex names and the proteins that interact with it
+  interact_lookup <-
+    read.csv(
+      '/Users/jamesboot/Documents/9.Genome Centre Files/interactVis/cellphonedb-data-4.0.0/data/interaction_input.csv'
+    )
+  
+  # Iterate through all complexes and update database list along to way
+  for (x in 1:nrow(complex_lookup)) {
+    # First: given a complex - return all the gene names of the proteins in the complex
+    # Get all the protein ids of proteins in the complex
+    complex_name <- complex_lookup$complex_name[x]
+    complex_prot <- c(
+      complex_lookup[complex_name,]$uniprot_1,
+      complex_lookup[complex_name,]$uniprot_2,
+      complex_lookup[complex_name,]$uniprot_3,
+      complex_lookup[complex_name,]$uniprot_4
+    )
+    # Remove blanks
+    complex_prot <- complex_prot[complex_prot != '']
+    # Now find the gene name
+    complex_genes <-
+      unique(gene_lookup$gene_name[gene_lookup$uniprot %in% complex_prot])
+    
+    # Second: given a complex - return the gene names of the proteins it interacts with (partner)
+    # Get the protein id of the partner
+    partner_prot <-
+      interact_lookup$partner_b[interact_lookup$partner_a == complex_name]
+    # Now find the gene names for partners
+    partner_genes <-
+      unique(gene_lookup$gene_name[gene_lookup$uniprot %in% partner_prot])
+    
+    # Add to the list (list is the database) - if length is greater than 0
+    if (length(partner_genes) > 0) {
+      
+      cellphonedb[[complex_name]] <- list(Complex_Genes = complex_genes,
+                                          Partner_Genes = partner_genes)
+      
+    } else {
+      next
+    }
+  }
+  
+  
+  
+  return(cellphonedb)
+  
+}
