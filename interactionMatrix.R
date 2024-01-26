9f8cda16b9d0a53cf8529978effcfea6cf79399f
James Boot <44063127+jamesboot@users.noreply.github.com>
2024-01-26 15:49:59 +0000

    Create SENDER RECEIVER matrices
    Dring interaction matrix creation, create tow matrices - 1 focussed on the sender - i.e. all spot 2s as columns, - and 1 focussed on the receiver - i.e. all spot 1s as columns

--- interactionMatrix.R
+++ interactionMatrix.R
@@ -1,6 +1,16 @@
-# Function to make a matrix with spot1 as columns and receptor-ligand as rows, interaction scores as values
+# Function to make matrices:
+# 1. RECEIVER MATRIX: Matrix with spot1 as columns, receptor-ligand as rows, interaction scores as values
+# Spot 1 is the receiver (where the receptor expression is taken from)
+# 2. SENDER MATRIX: Matrix with spot2 as columns, receptor-ligand as rows, interaction scores as values
+# Spot 2 is the sender (where the ligand expression is taken from)
+
+# Create function
 interactionMatrix <- function(AnnoInt) {
   
+  # Store the 2 matrices in a list
+  IntMatrices <- list()
+  
+  # 1. CREATE RECEIVER MATRIX: ----
   # Make a dummy list
   dumList <- list()
   
@@ -41,6 +51,54 @@ interactionMatrix <- function(AnnoInt) {
   # Get rid of NAs
   interactionMat[is.na(interactionMat)] <- 0
   
-  return(interactionMat)
+  # 2. ADD RECEIVER MATRIX TO LIST: ----
+  IntMatrices$ReceiverMat <- interactionMat
+  
+  # 3. CREATE SENDER MATRIX: ----
+  
+  # Make a dummy list
+  dumList <- list()
+  
+  # Create new receptor ligand column in AnnoInt
+  AnnoInt <- AnnoInt %>%
+    mutate(Receptor_Ligand = paste0(spot1_complex, '-', spot2_ligand))
+  
+  # For loop to go through each cell
+  # Use dplyr to extract and summarise interaction scores for all receptor-ligands
+  # Put the new dataframe for the spot into dummy list
+  c <- 1
+  for (bc in unique(AnnoInt$spot2)) {
+    message(paste0(
+      'Starting spot ',
+      c,
+      ' of ',
+      length(unique(AnnoInt$spot2)),
+      '. Progress: ',
+      round((c / length(
+        unique(AnnoInt$spot2)
+      ) * 100), digits = 1),
+      '%'
+    ))
+    int <- AnnoInt %>%
+      filter(spot2 == bc) %>%
+      group_by(Receptor_Ligand) %>%
+      summarise(Mean_Int = mean(interaction_score))
+    colnames(int)[2] <- bc
+    dumList[[bc]] <- int
+    c <- c + 1
+  }
+  
+  # Merge list elements
+  interactionMat <-
+    dumList %>% reduce(full_join, by = "Receptor_Ligand") %>%
+    column_to_rownames('Receptor_Ligand')
+  
+  # Get rid of NAs
+  interactionMat[is.na(interactionMat)] <- 0
+  
+  # 4. ADD RECEIVER MATRIX TO LIST:
+  IntMatrices$SenderMat <- interactionMat
+  
+  return(IntMatrices)
   
 }
