cffb03e3aea1f197001a464378d8bd6bc8cdb9ef
James Boot <james.boot.94@gmail.com>
2024-05-06 18:09:48 +0100

    Add new GLM test feature
    Add new statistical test - instead of wilcoxon test, fit a glm negative binomial model to the data with a null model, then one with the proposed model. Then compare the models to find a p-value - as would be performed in differential expression testing. New parameter into the function for specifying whteher wilcoxon or glm test should be performed.

--- differentialInteraction.R
+++ differentialInteraction.R
@@ -4,7 +4,8 @@
 differentialInteraction <- function(InteractionMatList,
                                     MetaList,
                                     Attribute,
-                                    Comparison) {
+                                    Comparison,
+                                    Test) {
   
   # Make a list for results to go in:
   diffIntRes <- list()
@@ -32,36 +33,85 @@ differentialInteraction <- function(InteractionMatList,
   # Check if there are enough replicates of groups
   if (sum(tTestDFFilt$Cluster == Comparison[1]) > 3 &
       sum(tTestDFFilt$Cluster == Comparison[2]) > 3) {
-    
     message('Starting statistical test for RECEIVERS.')
     
-    # Make dataframe for results
-    wilcoxRes <- data.frame()
-    
-    # For loop to perform test on all columns
-    for (x in c(2:(ncol(tTestDFFilt) - ncol(MetaList$ReceiverMeta)))) {
+    if (Test == 'Wilcox') {
       
-      # As data is not normally distributed - use wilcoxon
-      res <-
-        wilcox.test(tTestDFFilt[, x] ~ tTestDFFilt[, Attribute],
-                    data = tTestDFFilt,
-                    paired = F)
+      # Make dataframe for results
+      wilcoxRes <- data.frame()
       
-      # Adjust the p-value
-      FDR <-
-        p.adjust(res$p.value, method = 'fdr', n = length(c(2:(
-          ncol(tTestDFFilt) - ncol(MetaList$ReceiverMeta)
-        ))))
+      # For loop to perform test on all columns
+      for (x in c(2:(ncol(tTestDFFilt) - ncol(MetaList$ReceiverMeta)))) {
+        
+        # As data is not normally distributed - use wilcoxon
+        res <-
+          wilcox.test(tTestDFFilt[, x] ~ tTestDFFilt[, Attribute],
+                      data = tTestDFFilt,
+                      paired = F)
+        
+        # Adjust the p-value
+        FDR <-
+          p.adjust(res$p.value, method = 'fdr', n = length(c(2:(
+            ncol(tTestDFFilt) - ncol(MetaList$ReceiverMeta)
+          ))))
+        
+        # Put result into dataframe
+        wilcoxRes[colnames(tTestDFFilt)[x], 'Receptor-Ligand'] <-
+          colnames(tTestDFFilt)[x]
+        wilcoxRes[colnames(tTestDFFilt)[x], 'P-Value'] <- res$p.value
+        wilcoxRes[colnames(tTestDFFilt)[x], 'FDR'] <- FDR
+        
+      }
       
-      # Put result into dataframe
-      wilcoxRes[colnames(tTestDFFilt)[x], 'Receptor-Ligand'] <-
-        colnames(tTestDFFilt)[x]
-      wilcoxRes[colnames(tTestDFFilt)[x], 'P-Value'] <- res$p.value
-      wilcoxRes[colnames(tTestDFFilt)[x], 'FDR'] <- FDR
+      diffIntRes$ReceiverResults <- wilcoxRes
+      
+    } else if (Test == 'glm') {
+      
+      # Make dataframe for results
+      glmRes <- data.frame()
+      
+      # For loop to perform test on all columns
+      for (x in c(2:(ncol(tTestDFFilt) - ncol(MetaList$ReceiverMeta)))) {
+        
+        if (sum(tTestDFFilt[, x]) == 0) {
+          
+          next
+          
+        } else {
+          
+          # Fit null model
+          fit0 <- glm.nb(tTestDFFilt[, x] ~ 1,
+                         data = tTestDFFilt)
+          
+          # Fit proposed model
+          fit1 <- glm.nb(tTestDFFilt[, x] ~ tTestDFFilt[, Attribute],
+                         data = tTestDFFilt)
+          
+          # Compare fit of models
+          lrtResult <- lrtest(fit1, fit0)
+          
+          # Return p-value
+          p <- lrtResult$`Pr(>Chisq)`[2]
+          
+          # Adjust the p-value
+          FDR <-
+            p.adjust(p, method = 'fdr', n = length(c(2:(
+              ncol(tTestDFFilt) - ncol(MetaList$ReceiverMeta)
+            ))))
+          
+          # Put result into dataframe
+          glmRes[colnames(tTestDFFilt)[x], 'Receptor-Ligand'] <-
+            colnames(tTestDFFilt)[x]
+          glmRes[colnames(tTestDFFilt)[x], 'P-Value'] <- p
+          glmRes[colnames(tTestDFFilt)[x], 'FDR'] <- FDR
+          
+        }
+        
+      }
+      
+      diffIntRes$ReceiverResults <- glmRes
       
     }
-    
-    diffIntRes$ReceiverResults <- wilcoxRes
     
   } else {
     message('Not enough replicates in groups for statistical test. Skipping RECEIVERS.')
@@ -93,33 +143,83 @@ differentialInteraction <- function(InteractionMatList,
     
     message('Starting statistical test for SENDERS.')
     
-    # Make dataframe for results
-    wilcoxRes <- data.frame()
-    
-    # For loop to perform test on all columns
-    for (x in c(2:(ncol(tTestDFFilt) - ncol(MetaList$SenderMeta)))) {
+    if (Test == 'Wilcox') {
       
-      # As data is not normally distributed - use wilcoxon
-      res <-
-        wilcox.test(tTestDFFilt[, x] ~ tTestDFFilt[, Attribute],
-                    data = tTestDFFilt,
-                    paired = F)
+      # Make dataframe for results
+      wilcoxRes <- data.frame()
       
-      # Adjust the p-value
-      FDR <-
-        p.adjust(res$p.value, method = 'fdr', n = length(c(2:(
-          ncol(tTestDFFilt) - ncol(MetaList$SenderMeta)
-        ))))
+      # For loop to perform test on all columns
+      for (x in c(2:(ncol(tTestDFFilt) - ncol(MetaList$SenderMeta)))) {
+        
+        # As data is not normally distributed - use wilcoxon
+        res <-
+          wilcox.test(tTestDFFilt[, x] ~ tTestDFFilt[, Attribute],
+                      data = tTestDFFilt,
+                      paired = F)
+        
+        # Adjust the p-value
+        FDR <-
+          p.adjust(res$p.value, method = 'fdr', n = length(c(2:(
+            ncol(tTestDFFilt) - ncol(MetaList$SenderMeta)
+          ))))
+        
+        # Put result into dataframe
+        wilcoxRes[colnames(tTestDFFilt)[x], 'Receptor-Ligand'] <-
+          colnames(tTestDFFilt)[x]
+        wilcoxRes[colnames(tTestDFFilt)[x], 'P-Value'] <- res$p.value
+        wilcoxRes[colnames(tTestDFFilt)[x], 'FDR'] <- FDR
+        
+      }
       
-      # Put result into dataframe
-      wilcoxRes[colnames(tTestDFFilt)[x], 'Receptor-Ligand'] <-
-        colnames(tTestDFFilt)[x]
-      wilcoxRes[colnames(tTestDFFilt)[x], 'P-Value'] <- res$p.value
-      wilcoxRes[colnames(tTestDFFilt)[x], 'FDR'] <- FDR
+      diffIntRes$SenderResults <- wilcoxRes
+      
+    } else if (Test == 'glm') {
+      
+      # Make dataframe for results
+      glmRes <- data.frame()
+      
+      # For loop to perform test on all columns
+      for (x in c(2:(ncol(tTestDFFilt) - ncol(MetaList$SenderMeta)))) {
+        
+        if (sum(tTestDFFilt[, x]) == 0) {
+          
+          next
+          
+        } else {
+          
+          # Fit null model
+          fit0 <- glm.nb(tTestDFFilt[, x] ~ 1,
+                         data = tTestDFFilt)
+          
+          # Fit proposed model
+          fit1 <- glm.nb(tTestDFFilt[, x] ~ tTestDFFilt[, Attribute],
+                         data = tTestDFFilt)
+          
+          # Compare fit of models
+          lrtResult <- lrtest(fit1, fit0)
+          
+          # Return p-value
+          p <- lrtResult$`Pr(>Chisq)`[2]
+          
+          # Adjust the p-value
+          FDR <-
+            p.adjust(p, method = 'fdr', n = length(c(2:(
+              ncol(tTestDFFilt) - ncol(MetaList$SenderMeta)
+            ))))
+          
+          # Put result into dataframe
+          glmRes[colnames(tTestDFFilt)[x], 'Receptor-Ligand'] <-
+            colnames(tTestDFFilt)[x]
+          glmRes[colnames(tTestDFFilt)[x], 'P-Value'] <- p
+          glmRes[colnames(tTestDFFilt)[x], 'FDR'] <- FDR
+          
+        }
+        
+      }
+      
+      diffIntRes$SenderResults <- glmRes
       
     }
-    
-    diffIntRes$SenderResults <- wilcoxRes
     
   } else {
     message('Not enough replicates in groups for statistical test. Skipping SENDERS.')
